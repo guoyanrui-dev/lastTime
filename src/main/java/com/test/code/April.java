faee9f92f9807bf8e2fcf68f02de275ad3f1d91e
guoyanrui <wb_guoyanrui@kuaishou.com>
2022-03-25 16:34:09 +0800

    run

--- src/main/java/com/test/code/April.java
+++ src/main/java/com/test/code/April.java
@@ -3,5 +3,22 @@ package com.test.code;
 /**
  * @author guoyanrui on 2022/3/25
  */
-public class april {
+public class April implements Runnable {
+    @Override
+    public void run() {
+        int index=1;
+        for(;;){
+            System.out.println("这是第"+index+"信息");
+            if(index%100==0){
+                System.out.println("已经打印了"+index+"消息");
+                break;
+            }
+            index++;
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new April());
+        thread.start();
+    }
 }
