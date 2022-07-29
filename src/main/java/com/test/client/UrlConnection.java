7659c2ec3c8bc00a7a22319b818b016b15d78d9d
guoyanrui <gyr01411840@alibaba-inc.com>
2022-07-29 10:36:36 +0800

    hutool

--- src/main/java/com/test/client/UrlConnection.java
+++ src/main/java/com/test/client/UrlConnection.java
@@ -1,5 +1,7 @@
 package com.test.client;
 
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializerFeature;
@@ -9,27 +11,32 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.zip.CheckedOutputStream;
 
 public class UrlConnection {
-    public static JSONObject sendgetHttp(String url,@NotNull String... args) throws IOException {
+    public static JSONObject sendgetHttp(String url, String params) throws IOException {
         URL url1 = new URL(url);
-        HttpURLConnection connection  =(HttpURLConnection) url1.openConnection();
+        HttpURLConnection connection = (HttpURLConnection) url1.openConnection();
 //        for(int i=0;i<args.length;i++){
 //            String key = args[i].substring(0,args[i].indexOf("="));
 //            String value = args[i].substring(args[i].indexOf("=")+1);
 //            connection.setRequestProperty(key,value);
 //        }
+        connection.setRequestMethod("POST");
         connection.setDoOutput(true);
         connection.setDoInput(true);
-        connection.setRequestMethod("POST");
-        OutputStream outputStream  = connection.getOutputStream();
-        outputStream.write(args.toString().getBytes("utf-8"));
+        connection.connect();
+        OutputStream outputStream = connection.getOutputStream();
+        outputStream.write(params.getBytes("utf-8"));
+        outputStream.flush();
+        outputStream.close();
         InputStream inputStream = connection.getInputStream();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
         StringBuffer buffer = new StringBuffer();
         String temp;
-        while ((temp =bufferedReader.readLine())!=null){
+        while ((temp = bufferedReader.readLine()) != null) {
             buffer.append(temp);
             buffer.append("\r\n");
         }
@@ -41,8 +48,19 @@ public class UrlConnection {
     }
 
     public static void main(String[] args) throws IOException {
-        String url = "https://api.apishop.net/common/postcode/getAddrs";
-        JSONObject jsonObject = sendgetHttp(url,"apikey=hBcUuaPacd9143701e460219607c97752d070b3082954ac");
-        System.out.println(JSON.toJSONString(jsonObject, SerializerFeature.PrettyFormat));
+        String url = "http://11.164.95.218:8099/bin/search?auction&_sid_=1117251129629489&group_module=groupmeta_poi_detail&show_fields=all&sourcefrom=loadrunnertest&use_log=false&outfmt=json";
+//        Map<String,String> map = new HashMap();
+        JSONObject json = new JSONObject();
+        json.put("apikey", "hBcUuaPacd9143701e460219607c97752d070b3082954ac");
+        String param = "{\"apikey\":\"hBcUuaPacd9143701e460219607c97752d070b3082954ac\"}";
+//        map.put("apikey","hBcUuaPacd9143701e460219607c97752d070b3082954ac");
+//        JSONObject jsonObject = sendgetHttp(url,param);
+//        System.out.println(JSON.toJSONString(jsonObject, SerializerFeature.PrettyFormat));
+        String jsonObject = HttpRequest.get(url)
+                .execute()
+                .body();
+        JSONObject jsonObject1 = JSONObject.parseObject(jsonObject);
+        System.out.println(JSON.toJSONString(jsonObject1, SerializerFeature.PrettyFormat));
     }
+
 }
