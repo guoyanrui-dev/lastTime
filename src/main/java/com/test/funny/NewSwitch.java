f3a7dfb46b708c2f9c0ec74783a7032e813847f8
guoyanrui <gyr01411840@alibaba-inc.com>
2023-04-07 15:04:59 +0800

    lianxi

--- src/main/java/com/test/funny/NewSwitch.java
+++ src/main/java/com/test/funny/NewSwitch.java
@@ -1,7 +1,78 @@
 package com.test.funny;
 
+import java.util.Arrays;
+
 public class NewSwitch {
-    void getInfo(){
-        System.out.println("新");
+    //kuaipai
+    public static void quikly(int[] nums, int left, int right) {
+        if (left > right) return;
+        int i = left;
+        int j = right;
+        int base = nums[i];
+        while (i != j) {
+            while (nums[j] >= base && i < j) {
+                j--;
+            }
+            while (nums[i] <= base && i < j) {
+                i++;
+            }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        nums[left] = nums[i];
+        nums[i] = base;
+        quikly(nums, left, i - 1);
+        quikly(nums, i + 1, right);
+    }
+
+    //maopao
+    public static void bubble(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j + 1] < nums[j]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    //jiujiu
+    public static void jiujiu() {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + "*" + i + "=" + j * i + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    //erfenchazhao
+    public static int chazhaoerfeng(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                j = mid - 1;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 5, 32, 65, 786, 44, 88, 99, 543};
+//        quikly(nums, 0, nums.length - 1);
+        bubble(nums);
+        int index = chazhaoerfeng(nums, 88);
+        System.out.println(Arrays.toString(nums));
+        System.out.println(index);
+        jiujiu();
     }
 }
