3b5095b5cf5ef1869ee081fa30d4c67df63a8a18
guoyanrui <gyr01411840@alibaba-inc.com>
2023-05-19 09:53:10 +0800

    add

--- src/main/java/com/test/repeat/Yuliao.java
+++ src/main/java/com/test/repeat/Yuliao.java
@@ -3,6 +3,8 @@ package com.test.repeat;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import com.alibaba.fastjson.JSONObject;
+import org.testng.annotations.Test;
+import redis.clients.jedis.Jedis;
 
 
 public class Yuliao {
@@ -12,15 +14,15 @@ public class Yuliao {
     public static void main(String[] args) {
         String url = "http://lse2-search-data-factory.amap.test/us/queryLog";
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("queryString","url:\"/search/poi_brief\" and \"query_src=us-java-list-charging\"");
-        jsonObject.put("totalSize",30000);
-        jsonObject.put("serverName","lse2-us-pouch");
-        jsonObject.put("ossPath","oss://search-qa/guoyanrui/pouch.txt");
+        jsonObject.put("queryString","uri:\"/coupon/common/coupon/info\" and \"coupon_business=gasstation\"");
+        jsonObject.put("totalSize",10000);
+        jsonObject.put("serverName","lse2-us-bff-coupon-service");
+        jsonObject.put("ossPath","oss://search-qa/guoyanrui/bff_coupon.txt");
         HttpResponse httpResponse = HttpRequest.post(url)
                         .body(String.valueOf(jsonObject))
                         .execute();
         System.out.println(httpResponse.body());
+//        System.out.println(6.55*2775900);
     }
-
 
 }
